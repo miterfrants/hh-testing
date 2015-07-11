fb54a554824dd7555a23be418a8f4e8e80f695aa
Po-Hsiang Huang <miterfrants@gmail.com>
2015-07-11 23:36:59 +0800

    no message

--- src/test/java/NoLoginTest.java
+++ src/test/java/NoLoginTest.java
@@ -16,7 +16,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui;
 
 import java.net.URL;
 import java.util.Arrays;
